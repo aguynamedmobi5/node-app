8af9eed669fd8f03ee34df65e7356dd4ff9b3bbe
aguynamedmobi5 <52341566+aguynamedmobi5@users.noreply.github.com>
2024-05-17 08:03:02 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  default     = "hello-world-cluster"
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+  default     = "hello-world-app"
+}
+
+variable "desired_count" {
+  description = "Number of desired instances"
+  type        = number
+  default     = 1
+}
